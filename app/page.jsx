3642cc79c539cf51b8b398055406076265c2eaf8
dawidy10 <davidburaga10@gmail.com>
2024-10-16 19:59:31 +0300

    V1 ready to deploy

--- app/page.jsx
+++ app/page.jsx
@@ -9,6 +9,7 @@ import About from "@/components/about";
 import { Button } from "@/components/ui/button";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 import romans1 from "@/public/work/romans1.jpg";
@@ -71,7 +72,7 @@ export default function Home() {
 					}
 				/>
 			</div>
-			<div className={`${styles.background} px-8 md:px-32 py-4 relative`}>
+			<div className={`${styles.background} px-8 md:px-32 py-4 relative pb-32`}>
 				<h1 id="work" className={`${styles.title} text-white text-6xl`}>
 					Recent work.
 				</h1>
@@ -98,13 +99,13 @@ export default function Home() {
 							</CarouselItem>
 						))}
 					</CarouselContent>
-					<CarouselPrevious className="ml-8 md:ml-0" />
-					<CarouselNext className="mr-8 md:mr-0" />
+					<CarouselPrevious className="ml-8 bg-[#6E6E8C] border-0 md:ml-0" />
+					<CarouselNext className="mr-8 bg-[#6E6E8C] border-0 md:mr-0" />
 				</Carousel>
 
 				<h1 className={`${styles.title} mt-20 text-white text-6xl`}>Skills.</h1>
 				<div className="flex flex-col md:flex-row mt-20 items-center justify-evenly">
-					<Card className="bg-[#111112] border-none text-white w-64 text-center py-10">
+					<Card className="bg-[#111112] border-[#4F4F8B] text-white w-64 text-center py-10">
 						<CardHeader>
 							<div className="flex w-full justify-center">
 								<Avatar className="z-30 w-14 h-14">
@@ -128,7 +129,7 @@ export default function Home() {
 							</p>
 						</CardContent>
 					</Card>
-					<Card className="bg-[#111112] border-none text-white w-64 text-center py-10 mt-8 md:mt-0">
+					<Card className="bg-[#111112] border-[#88513F] text-white w-64 text-center py-10 mt-8 md:mt-0">
 						<CardHeader>
 							<div className="flex w-full justify-center">
 								<Avatar className="z-30 w-14 h-14">
@@ -154,6 +155,92 @@ export default function Home() {
 					</Card>
 				</div>
 				<h1 className={`${styles.title} mt-20 text-white text-6xl`}>Don't take my word for it.</h1>
+				<div className="flex flex-col mt-20 items-center justify-evenly">
+					<Card className="bg-[#111112] border-0 text-white w-100 md:w-2/3 text-center py-4 md:px-8 mt-8 md:mt-0">
+						<CardHeader>
+							<div className="ml-auto mr-auto w-20 h-20 rounded-full overflow-hidden">
+								<Image src="/work/refugiu.jpg" width={100} height={100} className="object-cover" />
+							</div>
+							<CardTitle>Refugiu pentru Suflet</CardTitle>
+							<CardDescription>refugiupentrusuflet.ro</CardDescription>
+						</CardHeader>
+						<CardContent>
+							<Tabs defaultValue="ro">
+								<TabsList className="bg-[#111112]">
+									<TabsTrigger className="bg-[#111112]" value="ro">
+										RO
+									</TabsTrigger>
+									<TabsTrigger className="bg-[#111112]" value="en">
+										EN
+									</TabsTrigger>
+								</TabsList>
+								<TabsContent value="ro">
+									<p className={`${styles.text} text-left md:text-lg mt-4`}>
+										"Colaborarea noastră cu David a fost una excelentă. I-am prezentat viziunea și
+										scopul asociației noastre, iar el a înțeles imediat direcția pe care o dorim.
+										Într-un timp foarte scurt, ne-a prezentat mai multe variante de logo, fiecare
+										reflectând în mod clar valorile noastre. După ce am ales designul care ne-a
+										plăcut cel mai mult, David a venit cu diverse idei de culori și nuanțe care să
+										ne reprezinte vizual cât mai bine. Profesionalismul și creativitatea lui ne-au
+										ajutat să ne consolidăm identitatea de brand și să ne prezentăm într-un mod
+										coerent și atractiv publicului nostru."
+									</p>
+								</TabsContent>
+								<TabsContent value="en">
+									<p className={`${styles.text} text-left`}></p>
+								</TabsContent>
+							</Tabs>
+						</CardContent>
+						<CardFooter>
+							<p className="text-sm text-gray-600">Web design | Web development</p>
+						</CardFooter>
+					</Card>
+					<Card className="bg-[#111112] border-0 text-white w-100 md:w-2/3 text-center py-4 mt-8">
+						<CardHeader>
+							<div className="ml-auto mr-auto w-20 h-20 rounded-full overflow-hidden">
+								<Image src="/anto.jpg" width={100} height={100} className="object-cover" />
+							</div>
+							<CardTitle>Anto Sticea</CardTitle>
+							<CardDescription>@antosticea</CardDescription>
+						</CardHeader>
+						<CardContent>
+							<Tabs defaultValue="ro">
+								<TabsList className="bg-[#111112]">
+									<TabsTrigger className="bg-[#111112]" value="ro">
+										RO
+									</TabsTrigger>
+									<TabsTrigger className="bg-[#111112]" value="en">
+										EN
+									</TabsTrigger>
+								</TabsList>
+								<TabsContent value="ro">
+									<p className={`${styles.text} text-left md:text-lg mt-4`}>
+										"Munca lui David a fost de un real folos pentru mine și afacerea mea. Cu
+										ajutorul site-ului creat de el, prospectii mei au ocazia sa ma cunoasca in doar
+										cateva minute si chiar sa ma contacteze in vederea colaborării. Totul este făcut
+										pe gustul meu, dar si actual, ceea ce imi creează o imagine de profesionist în
+										fața celor interesați de activitatea pe care o desfășor. Recomand cu încredere
+										serviciile lui dacă doriți să arătați cât mai bine în online și să ajungeți la
+										clienți de lux!"
+									</p>
+								</TabsContent>
+								<TabsContent value="en">
+									<p className={`${styles.text} text-left`}></p>
+								</TabsContent>
+							</Tabs>
+						</CardContent>
+						<CardFooter>
+							<p className="text-sm text-gray-600">Web design | Web development</p>
+						</CardFooter>
+					</Card>
+				</div>
+
+				<h1 className={`${styles.text} mt-32 text-center text-white text-2xl`}>
+					Need help with your own brand?
+				</h1>
+				<Link href="https://calendly.com/davidburaga10/discutie-1-la-1" target="blank">
+					<p className={`${styles.text} mt-20 underline text-center text-white text-2xl`}>Book a call.</p>
+				</Link>
 			</div>
 		</>
 	);
